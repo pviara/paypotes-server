d6539a00ad8c610b6dff8fa1684600ef66a21a93
Pierre Viara <pierre.viara@outlook.com>
2025-03-15 23:11:04 +0100

    add infra module and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { ContactModule } from '@contacts/contact.module';
 import { ExpenseModule } from '@expenses/expense.module';
 import { GroupModule } from '@groups/group.module';
+import { InfrastructureModule } from '@infra/infrastructure.module';
 import { Module } from '@nestjs/common';
 import { UserModule } from '@users/user.module';
 
 @Module({
-    imports: [ContactModule, ExpenseModule, GroupModule, UserModule],
+    imports: [
+        ContactModule,
+        ExpenseModule,
+        GroupModule,
+        InfrastructureModule,
+        UserModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ErrorFilter } from '@app/error-filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalFilters(new ErrorFilter());
 
-    await app.listen(process.env.PORT ?? 7001);
+    const configService = app.get(ConfigService);
+    const port = configService.get('APP_PORT');
+
+    await app.listen(port);
 }
 bootstrap();
