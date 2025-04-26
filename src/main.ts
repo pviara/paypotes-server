a54ad10a033d92a34f3730c4389ce348872acd52
Pierre Viara <pierre.viara@outlook.com>
2025-04-26 18:53:25 +0200

    use real rabbitmq in tests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ErrorFilter } from '@app/error-filter';
 import { ConfigService } from '@nestjs/config';
+import { ErrorFilter } from '@app/error-filter';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
