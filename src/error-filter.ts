deb486a8fc4ff44cb3f688006b7781e0f7eaecfe
Pierre Viara <pierre.viara@outlook.com>
2025-04-29 21:20:45 +0200

    rename error in error filter

--- src/error-filter.ts
+++ src/error-filter.ts
@@ -7,12 +7,12 @@ import {
 import { ContactExpenseNotFoundError } from '@expenses/application/queries/get-actor-contact-expense-by-id.handler';
 import { ContactNotFoundError } from '@contacts/application/get-actor-contact-with-balance-by-id.handler';
 import { ExpenseNotFoundError } from '@expenses/application/queries/get-actor-expense-by-id.handler';
+import { ExpenseUserNotFoundError } from '@expenses/application/commands/add-pair-expense.handler';
 import { ContactNotFoundInSavedList } from '@contacts/application/get-actor-contacts-with-balance.handler';
 import { GroupExpenseNotFoundError } from '@expenses/application/queries/get-actor-group-expense-by-id.handler';
 import { GroupNotFoundError } from '@groups/application/get-actor-group-with-balance-by-id.handler';
 import { GroupUserNotFoundError } from '@groups/application/create-group.handler';
 import { MemberNotInGroupError } from '@groups/domain/group';
-import { UserExpenseNotFoundError } from '@expenses/application/commands/add-pair-expense.handler';
 import { UserNotFoundWithNameError } from '@users/application/get-user-by-name.handler';
 
 @Catch(Error)
@@ -33,13 +33,13 @@ export class ErrorFilter implements ExceptionFilter {
         if (exception instanceof ExpenseNotFoundError) {
             throw new NotFoundException(exception.message);
         }
+        if (exception instanceof ExpenseUserNotFoundError) {
+            throw new NotFoundException(exception.message);
+        }
         if (exception instanceof ContactExpenseNotFoundError) {
             throw new NotFoundException(exception.message);
         }
         if (exception instanceof GroupExpenseNotFoundError) {
-            throw new NotFoundException(exception.message);
-        }
-        if (exception instanceof UserExpenseNotFoundError) {
             throw new NotFoundException(exception.message);
         }
         if (exception instanceof MemberNotInGroupError) {
