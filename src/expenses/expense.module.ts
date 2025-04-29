206caffbd53467aa09d6a8ab0e2206f376cd805b
Pierre Viara <pierre.viara@outlook.com>
2025-04-29 21:21:15 +0200

    rename module

--- src/expenses/expense.module.ts
+++ src/expenses/expense.module.ts
@@ -1,7 +1,7 @@
 import { AddGroupExpenseHandler } from '@expenses/application/commands/add-group-expense.handler';
 import { AddPairExpenseHandler } from '@expenses/application/commands/add-pair-expense.handler';
 import { ComputeActorBalanceHandler } from '@expenses/application/queries/compute-actor-balance.handler';
-import { ContactTaskMessengerModule } from '@infra/contact-task-managers/contact.task-messenger.module';
+import { ContactTaskManagerModule } from '@infra/contact-task-managers/contact.task-manager.module';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ExpenseController } from '@expenses/presentation/expense.controller';
 import { ExpenseRepositoryModule } from '@expenses/persistence/expense.repository-module';
@@ -19,7 +19,7 @@ import { UserRepositoryModule } from '@users/persistence/user.repository-module'
 @Module({
     controllers: [ExpenseController],
     imports: [
-        ContactTaskMessengerModule,
+        ContactTaskManagerModule,
         CqrsModule,
         ExpenseRepositoryModule,
         GroupRepositoryModule,
