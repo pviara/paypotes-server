ad38c3b8aa056e419c7b4fd1c56c53eb26b42554
Pierre Viara <pierre.viara@outlook.com>
2025-05-13 12:09:19 +0200

    refactor stakeholders share calculation

--- src/expenses/domain/stakeholders.ts
+++ src/expenses/domain/stakeholders.ts
@@ -1,6 +1,11 @@
 import { Member } from '@groups/domain/member';
 import { Stakeholder } from '@expenses/domain/stakeholder';
 
+type MemberWithTheirShare = {
+    member: Member;
+    share: number;
+};
+
 export class Stakeholders {
     private stakeholders = this.mapStakeholdersWithTheirShare();
 
@@ -14,23 +19,38 @@ export class Stakeholders {
     ) {}
 
     private mapStakeholdersWithTheirShare(): Array<Stakeholder> {
-        const roundedShare = Math.floor(this.balance / this.members.length);
-        const shares = Array(this.members.length).fill(roundedShare);
+        const shares = this.calculateShares();
+        const membersAndTheirShare = this.assignSharesToMembers(shares);
 
-        const distributed = shares.reduce((prev, next) => prev + next, 0);
-        const distribuable = this.balance - distributed;
-
-        for (let i = 0; i < distribuable; i++) {
-            shares[i]++;
-        }
-
-        const membersAndTheirShare = this.members.map((member, index) => [
-            member,
-            shares[index],
-        ]);
-
-        return membersAndTheirShare.map(([member, share]) =>
+        return membersAndTheirShare.map(({ member, share }) =>
             Stakeholder.from(member, share),
         );
+    }
+
+    private calculateShares(): Array<number> {
+        const shares = this.getRoundedShares();
+        const rest = this.calculateRestFrom(shares);
+
+        for (let i = 0; i < rest; i++) shares[i]++;
+        return shares;
+    }
+
+    private getRoundedShares(): Array<number> {
+        const roundedShare = Math.floor(this.balance / this.members.length);
+        return Array<number>(this.members.length).fill(roundedShare);
+    }
+
+    private calculateRestFrom(shares: Array<number>): number {
+        const distributed = shares.reduce((prev, next) => prev + next, 0);
+        return this.balance - distributed;
+    }
+
+    private assignSharesToMembers(
+        shares: Array<number>,
+    ): Array<MemberWithTheirShare> {
+        return this.members.map((member, index) => ({
+            member,
+            share: shares[index],
+        }));
     }
 }
