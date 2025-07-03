69bd72bcab51d92162aff7e7567a4d7ea82be94a
Pierre Viara <pierre.viara@outlook.com>
2025-07-04 00:07:03 +0200

    remove default users

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,24 @@
 import { AppModule } from '@app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ConsoleLogger } from '@nestjs/common';
-import { ContactRepository } from '@contacts/persistence/contact.repository';
-import { contactRepositoryToken } from '@contacts/persistence/contact.repository-provider';
-import { DEFAULT_USER } from '@test/doubles/auth/default-user';
 import { ErrorFilter } from '@app/error-filter';
-import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { User } from '@users/domain/user';
-import { UserRepository } from '@users/persistence/user.repository';
-import { userRepositoryToken } from '@users/persistence/user.repository-provider';
 
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.create(AppModule, { logger: new ConsoleLogger({ colors: false }) });
+    const app = await NestFactory.create(AppModule, {
+        logger: new ConsoleLogger({ colors: false }),
+    });
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.useGlobalFilters(new ErrorFilter());
     app.enableShutdownHooks();
     app.enableCors();
-
-    await createSampleUsersInLocalMode(app);
 
     const configService = app.get(ConfigService);
     const port = configService.get('APP_PORT');
     await app.listen(port, logListeningOn(port));
 }
 bootstrap();
-
-async function createSampleUsersInLocalMode(
-    app: INestApplication,
-): Promise<void> {
-    const configService = app.get(ConfigService);
-    const environment = configService.get('APP_ENVIRONMENT');
-
-    if (environment === 'local') {
-        const users = [
-            new User({
-                id: '6852eafd-5179-46f2-9425-b4938b9eb3c0',
-                firstname: 'Peter',
-                lastname: 'Parker',
-                email: 'peter.parker@test.com',
-                avatarUrl:
-                    'https://gravatar.com/avatar/6d47aeeb1c5ea9a4f9f7ea7ecc36a721?s=800&d=mp&r=x',
-            }),
-            new User({
-                id: 'b6c614d7-7ac1-4822-b8e1-71c4b71051e1',
-                firstname: 'Bruce',
-                lastname: 'Wayne',
-                email: 'bruce.wayne@test.com',
-                avatarUrl:
-                    'https://gravatar.com/avatar/6d47aeeb1c5ea9a4f9f7ea7ecc36a721?s=800&d=mp&r=x',
-            }),
-            new User({
-                id: DEFAULT_USER.getId(),
-                firstname: 'Clark',
-                lastname: 'Kent',
-                email: 'clark.kent@test.com',
-                avatarUrl:
-                    'https://gravatar.com/avatar/6d47aeeb1c5ea9a4f9f7ea7ecc36a721?s=800&d=mp&r=x',
-            }),
-        ];
-        const userRepo = app.get<UserRepository>(userRepositoryToken);
-        for (const user of users) await userRepo?.create(user);
-
-        const contactRepo = app.get<ContactRepository>(contactRepositoryToken);
-        await contactRepo.addRelationshipsBetween([DEFAULT_USER, users[0]]);
-    }
-}
 
 function logListeningOn(port: string): () => void {
     return () =>
