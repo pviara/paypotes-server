f5c4cbb3a42ddcf9cda7dc7103f9fde145bdc73b
Pierre Viara <pierre.viara@outlook.com>
2025-08-06 21:02:06 +0200

    add type

--- src/expenses/domain/stakeholder/shares.ts
+++ src/expenses/domain/stakeholder/shares.ts
@@ -1,17 +1,16 @@
 import { Person } from '@expenses/domain/stakeholder/stakeholder';
 
+type CalculateShares = {
+    balance: number;
+    persons: Array<Person>;
+};
+
 export class Shares {
     private constructor(private value: Array<number>) {}
 
-    static calculate(data: {
-        balance: number;
-        persons: Array<Person>;
-    }): Array<number> {
-        const shares = this.calculateRoundedSharesFrom(
-            data.balance,
-            data.persons,
-        );
-        const rest = this.calculateRestFrom(data.balance, shares);
+    static calculate({ balance, persons }: CalculateShares): Array<number> {
+        const shares = this.calculateRoundedSharesFrom(balance, persons);
+        const rest = this.calculateRestFrom(balance, shares);
         const fullyDistributedShares = this.redistribute(rest, shares);
         return new Shares(fullyDistributedShares).value;
     }
