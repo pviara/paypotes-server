413329f398a553934ccbe19d2edbb7d929492a31
Pierre Viara <pierre.viara@outlook.com>
2025-08-07 19:32:58 +0200

    update member table

--- src/infrastructure/postgres/migrations/0004-member.ts
+++ src/infrastructure/postgres/migrations/0004-member.ts
@@ -4,18 +4,17 @@ export async function up(knex: Knex): Promise<void> {
     const exists = await knex.schema.hasTable('members');
     if (!exists) {
         await knex.schema.createTable('members', (table) => {
-            table.uuid('id', { primaryKey: true }).notNullable();
-
-            table.uuid('user_id').notNullable();
+            table.uuid('id').notNullable();
             table.uuid('group_id').notNullable();
 
-            table.foreign('user_id').references('id').inTable('users');
+            table.foreign('id').references('id').inTable('users');
             table.foreign('group_id').references('id').inTable('groups');
+
+            table.unique(['id', 'group_id']);
         });
 
         await knex('members').insert({
             id: process.env.DEFAULT_UUID,
-            user_id: process.env.DEFAULT_UUID,
             group_id: process.env.DEFAULT_UUID,
         });
     }
