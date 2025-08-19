090c12af60f305d60f4fb245541325b5e7629929
Pierre Viara <pierre.viara@outlook.com>
2025-08-19 15:07:19 +0200

    update stakeholders table

--- src/infrastructure/postgres/migrations/0006-stakeholder.ts
+++ src/infrastructure/postgres/migrations/0006-stakeholder.ts
@@ -4,20 +4,19 @@ export async function up(knex: Knex): Promise<void> {
     const exists = await knex.schema.hasTable('stakeholders');
     if (!exists) {
         await knex.schema.createTable('stakeholders', (table) => {
-            table.uuid('id', { primaryKey: true }).notNullable();
+            table.uuid('id').notNullable();
+            table.uuid('expense_id').notNullable();
             table.boolean('creditor').notNullable().defaultTo(false);
             table.integer('share').notNullable().defaultTo(0);
 
-            table.uuid('expense_id').notNullable();
-            table.uuid('user_id').notNullable();
-
+            table.foreign('id').references('id').inTable('users');
             table.foreign('expense_id').references('id').inTable('expenses');
-            table.foreign('user_id').references('id').inTable('users');
+
+            table.unique(['id', 'expense_id']);
         });
 
         await knex('stakeholders').insert({
             id: process.env.DEFAULT_UUID,
-            user_id: process.env.DEFAULT_UUID,
             expense_id: process.env.DEFAULT_UUID,
         });
     }
