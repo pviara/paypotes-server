87961508ceb1bef37f9d3074804d2b44cf261cbe
Pierre Viara <pierre.viara@outlook.com>
2025-09-08 19:21:19 +0200

    enhance error filter system

--- src/infrastructure/logger/log.decorator.ts
+++ src/infrastructure/logger/log.decorator.ts
@@ -12,7 +12,7 @@ export const Log = (level: LogLevel) => {
             ...args: Array<unknown>
         ): Promise<unknown> {
             if (process.env.APP_ENVIRONMENT === 'test')
-                return await decoratedMethod.apply(this, args);
+                return decoratedMethod.apply(this, args);
 
             const context = target.constructor.name;
             this.logger.setContext(context);
