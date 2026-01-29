b26c7767879f513890fce0deb76735f454699ca8
Pierre Viara <pierre.viara@outlook.com>
2026-01-29 19:45:59 +0100

    insert relationships once

--- src/contacts/persistence/contact.postgres-repository.ts
+++ src/contacts/persistence/contact.postgres-repository.ts
@@ -18,20 +18,27 @@ export class ContactPostgresRepository implements ContactRepository {
 
     @Log('debug')
     async addRelationshipsBetween(users: Array<User>): Promise<void> {
-        for (const user of users) {
-            const otherUsers = this.getOtherUsersThan(user, users);
-            for (const otherUser of otherUsers) {
-                const exists = await this.existsBetween(user, otherUser);
-                if (exists) continue;
+        const relationships: Array<{ user_a_id: string; user_b_id: string }> =
+            [];
 
-                await this.knex
-                    .insert({
-                        user_a_id: user.getId(),
-                        user_b_id: otherUser.getId(),
-                    })
-                    .into(Table.Relationships);
+        for (let i = 0; i < users.length; i++) {
+            for (let j = i + 1; j < users.length; j++) {
+                relationships.push({
+                    user_a_id: users[i].getId(),
+                    user_b_id: users[j].getId(),
+                });
             }
         }
+
+        if (relationships.length === 0) {
+            return;
+        }
+
+        await this.knex
+            .insert(relationships)
+            .into(Table.Relationships)
+            .onConflict(['user_a_id', 'user_b_id'])
+            .ignore();
     }
 
     @Log('debug')
@@ -125,27 +132,6 @@ export class ContactPostgresRepository implements ContactRepository {
             .limit(20);
 
         return this.mapContactsFrom(contacts);
-    }
-
-    private getOtherUsersThan(user: User, users: Array<User>): Array<User> {
-        return users.filter((otherUser) => user.getId() !== otherUser.getId());
-    }
-
-    private async existsBetween(userA: User, userB: User): Promise<boolean> {
-        const relationship = await this.knex
-            .select()
-            .from(Table.Relationships)
-            .where((subQueryBuilder) =>
-                subQueryBuilder
-                    .where('user_a_id', userA.getId())
-                    .andWhere('user_b_id', userB.getId()),
-            )
-            .orWhere((subQueryBuilder) =>
-                subQueryBuilder
-                    .where('user_a_id', userB.getId())
-                    .andWhere('user_b_id', userA.getId()),
-            );
-        return relationship.length > 0;
     }
 
     private mapContactsFrom(records: Array<ContactRecord>): Array<Contact> {
